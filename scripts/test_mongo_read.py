4a8fc93c9404d7c5c74b56c0a748b623afc787e5
Yamma Snake <mailroom@nihdatacommons.com>
2018-10-26 16:53:43 -0700

    add mongo test scripts

---
+++ scripts/test_mongo_read.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+
+"""
+Test Read
+
+Tests the ability to read records from the database.
+"""
+
+#client = MongoClient('10.6.0.1', 27017)
+client = MongoClient('127.0.0.1', 27017)
+db = client.test_database
+collection = db.test_collection
+
+for doc in collection.find():
+    print(doc)
+
